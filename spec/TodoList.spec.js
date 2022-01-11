cbcc20d84b453283d672897501d284527d8a261c
Elvis Onobhayedo <elvisonob@yahoo.com>
2022-01-11 08:39:14 +0000

    current

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -1,22 +1,65 @@
 const TodoList = require("../src/TodoList.js")
 
 describe("TodoList", () => {
-  let todoList
+    let todoList
 
-  beforeEach(() => {
-    todoList = new TodoList()
-  })
+    beforeEach(() => {
+        todoList = new TodoList()
+    })
 
-  it("creates a todo item", () => {
-    // set up
-    const expected = {
-      id: 1,
-      text: "turn the heating on!",
-      status: "incomplete"
-    }
-    // execute
-    const result = todoList.create("turn the heating on!")
-    // verify
-    expect(result).toEqual(expected)
-  })
+    it("creates a todo item", () => {
+        // set up
+        const expected = {
+                id: 1,
+                text: "turn the heating on!",
+                status: "incomplete"
+            }
+            // execute
+        const result = todoList.create("turn the heating on!")
+            // verify
+        expect(result).toEqual(expected)
+    })
+
+    //Get all todo items
+    it("Get todo list", () => {
+        // set up
+        const expected = [{
+                id: 1,
+                text: "turn the heating on!",
+                status: "incomplete"
+            },
+            {
+                id: 2,
+                text: "making a meal",
+                status: "incomplete"
+            }
+        ]
+
+        // execute
+        todoList.create('turn the heating on!')
+        todoList.create('making a meal')
+        const result = todoList.activity()
+
+        // verify
+        expect(result).toEqual(expected)
+    })
+
+    //Set a todo completed by its ID
+    it("Set todo to complete", () => {
+        // set up
+        const expected = [{
+                id: 1,
+                text: "turn the heating on!",
+                status: "complete"
+            },
+
+        ]
+
+        // execute
+        todoList.create('turn the heating on!')
+        const result = todoList.setToComplete(1)
+
+        // verify
+        expect(result).toEqual(expected)
+    })
 })
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,32 @@
 class TodoList {
+    constructor() {
+        this.todoList = [] //one, two, three
+    }
+
+    create(description) {
+        const todo = {
+            id: this.todoList.length + 1,
+            text: description,
+            status: "incomplete"
+        }
+        this.todoList.push(todo)
+        return todo
+
+    }
+
+    activity() {
+        return this.todoList
+    }
+    setToComplete(id) {
+        console.log('before computer says ', this.todoList[id - 1].status)
+        this.todoList[id - 1].status = 'complete'
+        console.log('after computer says ', this.todoList[id - 1].status)
+        return this.todoList
+
+    }
 
 }
+
+module.exports = TodoList
 
 module.exports = TodoList
